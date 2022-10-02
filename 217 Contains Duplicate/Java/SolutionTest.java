3d9e679ebc7906b705f6aff00227f792a49ebffe
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2022-10-02 11:49:42 -0600

    Add java tests for 217 for min and max length

--- 217 Contains Duplicate/Java/SolutionTest.java
+++ 217 Contains Duplicate/Java/SolutionTest.java
@@ -12,4 +12,21 @@ public class SolutionTest {
         assertEquals(false, Solution.containsDuplicate(example2));
         assertEquals(true, Solution.containsDuplicate(example3));
     }
+
+    @Test
+    public void testMaxLengthNums() {
+        int[] nums = new int[100000];
+        for (int i = 1; i < nums.length + 1; i++) {
+            nums[i - 1] = i;
+        }
+
+        assertEquals(false, Solution.containsDuplicate(nums));
+    }
+
+    @Test
+    public void testMinLengthNums() {
+        int[] nums = { 1 };
+
+        assertEquals(false, Solution.containsDuplicate(nums));
+    }
 }
