725d99e34f66f343a92897d506246a1e5819810b
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-02-27 23:19:53 -0700

    Create JavaScript template for solutions

---
+++ JavaScriptTemplate/solution.js
@@ -0,0 +1,5 @@
+const solution = () => {
+  return true;
+};
+
+module.exports = solution;
---
+++ JavaScriptTemplate/solution.test.js
@@ -0,0 +1,7 @@
+const solution = require('./solution');
+
+describe('Test solution', () => {
+  test('solution returns true', () => {
+    expect(solution()).toBe(true);
+  });
+});
