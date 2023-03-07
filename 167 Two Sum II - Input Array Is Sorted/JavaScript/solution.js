440e6cf2f75ba609c5f01f6206fe186a91b97c38
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-03-07 13:55:37 -0700

    Optimized JavaScript solution for 167

--- 167 Two Sum II - Input Array Is Sorted/JavaScript/solution.js
+++ 167 Two Sum II - Input Array Is Sorted/JavaScript/solution.js
@@ -4,44 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-  // strategy: use two pointers to traverse the numbers array and find the pair
-  // first pointer moves forward one at a time
-  // second pointer moves forward until it passes the target, then goes backwards if necessary
-  if (numbers.length === 2) return [1, 2];
+  // strategy: use a left and right pointer to find the pair
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
 
-  let first = 0;
-  let second = 1;
-  let forwards = true;
-
-  while (numbers[first] + numbers[second] !== target) {
-    if (first === second) {
-      first = second;
-      second++;
-    }
-
-    if (second === numbers.length) {
-      first++;
-      second = numbers.length - 1;
-    } else {
-      if (forwards) {
-        if (numbers[first] + numbers[second] < target) second++;
-        else {
-          first++;
-          if (first === second) second++;
-          if (numbers[first] + numbers[second] > target) forwards = false;
-        }
-      } else {
-        if (numbers[first] + numbers[second] > target) second--;
-        else {
-          first++;
-          if (first === second) second++;
-          if (numbers[first] + numbers[second] < target) forwards = true;
-        }
-      }
-    }
+    if (sum < target) left++;
+    else if (sum > target) right--;
+    else return [left + 1, right + 1];
   }
-
-  return [first + 1, second + 1];
 };
 
 module.exports = twoSum;
