eca80a3dd7250b7135b4079894b84690555bde6c
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-03-24 20:00:43 -0600

    Add optimized JS solution for 213

--- 213 House Robber II/JavaScript/solution.js
+++ 213 House Robber II/JavaScript/solution.js
@@ -6,31 +6,23 @@ var rob = function (nums) {
   // use the solution from House Robber I twice
   // check the case where you take the first house and skip the last, or vice versa, and pick the max
 
-  // because we skip the first and last in each case, we need a base case for length 1
-  if (nums.length === 1) return nums[0];
+  return Math.max(
+    nums[0],
+    helper(nums.slice(1, nums.length)),
+    helper(nums.slice(0, nums.length - 1))
+  );
+};
 
+const helper = (nums) => {
   let back2 = 0;
   let back1 = 0;
-  for (const [index, num] of nums.entries()) {
-    if (index === nums.length - 1) continue;
-
-    let temp = Math.max(num + back2, back1);
+  for (const num of nums) {
+    let temp = Math.max(back2 + num, back1);
     back2 = back1;
     back1 = temp;
   }
 
-  let firstSolution = back1;
-  back2 = 0;
-  back1 = 0;
-  for (const [index, num] of nums.entries()) {
-    if (index === 0) continue;
-
-    let temp = Math.max(num + back2, back1);
-    back2 = back1;
-    back1 = temp;
-  }
-
-  return Math.max(firstSolution, back1);
+  return back1;
 };
 
 module.exports = rob;
