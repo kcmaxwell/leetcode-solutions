76a73759ac23115634d739a1d3232929cd6ac01c
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-03-27 17:10:30 -0600

    Add some comments to JS 64

--- 64 Minimum Path Sum/JavaScript/solution.js
+++ 64 Minimum Path Sum/JavaScript/solution.js
@@ -7,19 +7,24 @@ var minPathSum = function (grid) {
   let col = grid[0].length;
 
   // result stores the min values for the current row
+  // result's final size will be col, the zero it starts with can be thought of as position (-1, 0)
   let result = [0];
 
-  // loop along each row, going down 1 column at a time
+  // go down 1 column at a time, across each row, storing min values in result
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
-      if (i === 0)
-        result.push(result[j] + grid[i][j]); // at the start of each row
+      // on the first row only, we will fill result with values
+      // at (0, 0), we look at (-1, 0), which is 0, and add grid[0][0]
+      if (i === 0) result.push(result[j] + grid[i][j]);
+      // at the start of the each row, add the min value above to the current point's value
       else if (j === 0) result[j + 1] += grid[i][j];
-      // along the first row only, just add the current value, no value above to worry about
+      // pick the minimum between the value above (j + 1) and the value to the left (j)
       else result[j + 1] = Math.min(result[j], result[j + 1]) + grid[i][j];
     }
   }
 
+  // return the value at (row, col)
+  // remember, result is length col now, result[0] is at (-1, row)
   return result[col];
 };
 
