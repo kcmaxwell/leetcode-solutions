9fb80cc815667cea4304a7780b5147dd112b829d
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-03-31 16:35:57 -0600

    Optimize for space on JS solution for 695

--- 695 Max Area of Island/JavaScript/solution.js
+++ 695 Max Area of Island/JavaScript/solution.js
@@ -5,7 +5,6 @@
 var maxAreaOfIsland = function (grid) {
   // iterate through the grid until you find a 1, then add up all tiles adjacent and check if it is a new max
   let max = 0;
-  const visited = new Set();
 
   const search = (x, y) => {
     // if out of bounds, return 0
@@ -14,11 +13,10 @@ var maxAreaOfIsland = function (grid) {
     // if not land, return 0
     if (grid[x][y] !== 1) return 0;
 
-    // if already visited, return 0
-    if (visited.has(`${x},${y}`)) return 0;
+    // change the value of the grid space to show it has been visited
+    grid[x][y] = '*';
 
     // otherwise, return 1 plus the values in all 4 directions around
-    visited.add(`${x},${y}`);
     return (
       1 +
       search(x + 1, y) +
@@ -31,7 +29,7 @@ var maxAreaOfIsland = function (grid) {
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[0].length; y++) {
       // check the area of the island if we haven't yet seen it
-      if (grid[x][y] === 1 && !visited.has(`${x},${y}`)) {
+      if (grid[x][y] === 1) {
         max = Math.max(max, search(x, y));
       }
     }
