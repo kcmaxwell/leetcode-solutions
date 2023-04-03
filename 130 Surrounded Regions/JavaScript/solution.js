7eae39f52dabec2a071baae8fda95d8c9b0c9f9e
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-04-03 11:31:31 -0600

    Add working JS solution for 130

--- 130 Surrounded Regions/JavaScript/solution.js
+++ 130 Surrounded Regions/JavaScript/solution.js
@@ -5,8 +5,6 @@
 var solve = function (board) {
   // for each found O, find all adjacent Os and save their positions
   // if none of the Os are adjacent to the border, flip all Os to X
-  const region = [];
-
   const isSurrounded = (x, y) => {
     // if out of bounds, return false
     if (x < 0 || y < 0 || x >= board.length || y >= board[0].length)
@@ -16,24 +14,27 @@ var solve = function (board) {
     if (board[x][y] === 'X' || board[x][y] === '*') return true;
 
     // if tile is an O, add x,y to region, temporarily change it to *, then check adjacent tiles
-    region.push({ x, y });
     board[x][y] = '*';
     const result =
       isSurrounded(x + 1, y) &&
       isSurrounded(x - 1, y) &&
       isSurrounded(x, y + 1) &&
       isSurrounded(x, y - 1);
-    board[x][y] = 'O';
     return result;
   };
 
   for (let x = 0; x < board.length; x++) {
     for (let y = 0; y < board[0].length; y++) {
       if (board[x][y] === 'O') {
-        region.splice(0, region.length);
+        let replace = 'O';
         if (isSurrounded(x, y)) {
-          for (const tile of region) {
-            board[tile.x][tile.y] = 'X';
+          replace = 'X';
+        }
+        for (let i = 0; i < board.length; i++) {
+          for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] === '*') {
+              board[i][j] = replace;
+            }
           }
         }
       }
