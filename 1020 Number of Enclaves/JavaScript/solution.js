1798f23f5986dd74e6f5fcbb90c56ff22a46d5e3
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-04-06 20:22:41 -0600

    Add BFS for 1020, slower time, but less memory

--- 1020 Number of Enclaves/JavaScript/solution.js
+++ 1020 Number of Enclaves/JavaScript/solution.js
@@ -18,13 +18,34 @@ var numEnclaves = function (grid) {
     }
   };
 
+  const bfs = (x, y) => {
+    const queue = [{ x, y }];
+    while (queue.length > 0) {
+      const cell = queue.shift();
+      if (
+        cell.x < 0 ||
+        cell.y < 0 ||
+        cell.x >= grid.length ||
+        cell.y >= grid[0].length
+      )
+        continue;
+      if (grid[cell.x][cell.y] === 1) {
+        grid[cell.x][cell.y] = 0;
+        queue.push({ x: cell.x + 1, y: cell.y });
+        queue.push({ x: cell.x - 1, y: cell.y });
+        queue.push({ x: cell.x, y: cell.y - 1 });
+        queue.push({ x: cell.x, y: cell.y + 1 });
+      }
+    }
+  };
+
   for (let x = 0; x < grid.length; x++) {
-    if (grid[x][0] === 1) replace(x, 0);
-    if (grid[x][grid[0].length - 1]) replace(x, grid[0].length - 1);
+    if (grid[x][0] === 1) bfs(x, 0);
+    if (grid[x][grid[0].length - 1]) bfs(x, grid[0].length - 1);
   }
   for (let y = 0; y < grid[0].length; y++) {
-    if (grid[0][y] === 1) replace(0, y);
-    if (grid[grid.length - 1][y] === 1) replace(grid.length - 1, y);
+    if (grid[0][y] === 1) bfs(0, y);
+    if (grid[grid.length - 1][y] === 1) bfs(grid.length - 1, y);
   }
 
   for (let x = 1; x < grid.length - 1; x++) {
