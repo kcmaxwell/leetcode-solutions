c55a4add7256ad09942c3d15e2d5c6b4010aedc9
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-04-07 12:38:50 -0600

    Add better JS solution for 322

--- 322 Coin Change/JavaScript/solution.js
+++ 322 Coin Change/JavaScript/solution.js
@@ -4,36 +4,19 @@
  * @return {number}
  */
 var coinChange = function (coins, amount) {
-  let result = Infinity;
-  const memo = new Map();
+  // dp[i] represents the least amount of coins that can make the value equal to i
+  const dp = Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
 
-  const dfs = (count, total) => {
-    // if the current total matches the amount, update result if it is a new min
-    if (total === amount) {
-      result = Math.min(count, result);
-      return;
-    }
-
-    // if the total passed the amount, or the coin count is past the current min, return
-    if (total > amount || count >= result) return;
-
-    // if the memo has this total with the same or smaller num of coins, return, else update memo
-    if (memo.has(total) && memo.get(total) <= count) {
-      return;
-    } else {
-      memo.set(total, count);
-    }
-
-    // try dfs on each coin type
+  for (let i = 1; i <= amount; i++) {
     for (const coin of coins) {
-      dfs(count + 1, total + coin);
+      if (i - coin >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
     }
-  };
+  }
 
-  dfs(0, 0);
-
-  if (result === Infinity) return -1;
-  return result;
+  return dp[amount] === Infinity ? -1 : dp[amount];
 };
 
 module.exports = coinChange;
