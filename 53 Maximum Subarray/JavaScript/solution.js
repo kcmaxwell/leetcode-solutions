a878060de4ef1a912a14629aaac01c928b67b05f
Kristopher Maxwell <kristophermaxwellc@gmail.com>
2023-04-15 21:20:47 -0600

    Add divide and conquer version for JS 53

--- 53 Maximum Subarray/JavaScript/solution.js
+++ 53 Maximum Subarray/JavaScript/solution.js
@@ -3,19 +3,40 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let result = nums[0];
-  let temp = 0;
-  for (let i = 0; i < nums.length; i++) {
-    temp += nums[i];
-    if (temp > result) {
-      result = temp;
-    }
-    if (temp < 0) {
-      temp = 0;
-    }
-  }
+  const findMax = (l, r) => {
+    // base case, return single value
+    if (l === r) return nums[l];
 
-  return result;
+    let mid = Math.floor((l + r) / 2);
+    let leftSum = findMax(l, mid);
+    let rightSum = findMax(mid + 1, r);
+    let crossingSum = findCrossingSum(l, mid, r);
+
+    return Math.max(leftSum, rightSum, crossingSum);
+  };
+
+  const findCrossingSum = (l, mid, r) => {
+    // a crossing sum crosses the left and right side
+    // mid is on the left side, mid + 1 is on the right, and the crossing sum must contain both
+    // start search for largest sum across both with these two values
+    let sum = 0;
+    let maxLeft = -Infinity;
+    for (let i = mid; i >= l; i--) {
+      sum += nums[i];
+      maxLeft = Math.max(maxLeft, sum);
+    }
+
+    sum = 0;
+    let maxRight = -Infinity;
+    for (let i = mid + 1; i <= r; i++) {
+      sum += nums[i];
+      maxRight = Math.max(maxRight, sum);
+    }
+
+    return maxLeft + maxRight;
+  };
+
+  return findMax(0, nums.length - 1);
 };
 
 module.exports = maxSubArray;
